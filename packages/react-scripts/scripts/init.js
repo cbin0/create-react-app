694bae56e6f5a6574b39eb1e2b45e1c7b9cf720e
cuibin <cuibin@admaster.com.cn>
2019-02-25 18:21:36 +0800

    rm allCommand

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -154,29 +154,17 @@ module.exports = function(
     }
   }
 
-  let allCommand
   let command;
   let args;
 
   if (useYarn) {
-    allCommand = 'yarn install';
     command = 'yarnpkg';
     args = ['add'];
   } else {
-    allCommand = 'npm install'
     command = 'npm';
     args = ['install', '--save', verbose && '--verbose'].filter(e => e);
   }
   args.push('react', 'react-dom');
-
-  // install all dependencies
-  console.log(`Installing all dependencies using ${allCommand}...`);
-  console.log();
-  const proc = spawn.sync(allCommand, [], { stdio: 'inherit' });
-  if (proc.status !== 0) {
-    console.error(`\`${allCommand}\` failed`);
-    return;
-  }
 
   // Install additional template dependencies, if present
   const templateDependenciesPath = path.join(
